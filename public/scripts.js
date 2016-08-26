eb65663fb2f16a92828c1ceff988d75ad499410c
Russ <russell.elfenbein@gmail.com>
2016-08-26 10:31:20 +0000

    complete

--- public/scripts.js
+++ public/scripts.js
@@ -2,8 +2,6 @@ var pic = document.getElementById('pic');
 var btn = document.getElementById('submit');
 
 var filename = document.getElementById('filename');
-var size = document.getElementById('size');
-filename.innerHTML = "fuck";
 
 pic.addEventListener("change", function(e){
    
@@ -11,6 +9,9 @@ pic.addEventListener("change", function(e){
 
 btn.addEventListener('click', function(e){
     console.log('name',pic.files[0].name);
-    filename.innerHTML = pic.files[0].name + ".";
-    size.innerHTML = (Math.floor(100*pic.files[0].size/1024)/100) + 'kB';
+    var res = {
+        name: pic.files[0].name + ".",
+        size: (Math.floor(100*pic.files[0].size/1024)/100) + 'kB'
+    };
+    filename.innerHTML = '{\n\tname: '+res.name+'\n\tsize: '+res.size+'\n}';
 });
